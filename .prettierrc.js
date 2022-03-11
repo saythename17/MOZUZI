d313f335132bd7fe5e094bfff6c69f2f97431bf8
fanxy_on <fanxy_on@alphacircle.co.kr>
2022-03-11 23:29:33 +0900

    initial

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  bracketSpacing: true, // 객체 리터럴에서 괄호에 공백 삽입 여부
+  jsxBracketSameLine: false, // JSX의 마지막 `>`를 다음 줄로 내릴지 여부
+  arrowParens: 'always', // 화살표 함수 괄호 사용 방식
+  singleQuote: true,
+  trailingComma: 'none', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+  endOfLine: 'auto', // EoF 방식, OS별로 처리 방식이 다름
+  htmlWhitespaceSensitivity: 'css', // HTML 공백 감도 설정
+  insertPragma: false,
+  jsxSingleQuote: true, // JSX에 singe 쿼테이션 사용 여부
+  printWidth: 80, // 줄 바꿈 폭 길이
+  proseWrap: 'preserve', // markdown 텍스트의 줄바꿈 방식 (v1.8.2)
+  quoteProps: 'as-needed', // 객체 속성에 쿼테이션 적용 방식
+  requirePragma: false,
+  semi: false, // 세미콜론 사용 여부
+  singleQuote: true, // single 쿼테이션'' 사용 여부
+  tabWidth: 2, // 탭 너비
+  useTabs: false, // 탭 사용 여부
+  vueIndentScriptAndStyle: false
+}
